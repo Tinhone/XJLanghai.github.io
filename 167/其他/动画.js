73b64adc05bdd9afafa7b31333a2bcfb15ee1a81
XJLanghai <654126913@qq.com>
2021-07-23 07:28:56 +0800

    master

---
+++ 167/其他/动画.js
@@ -0,0 +1,12 @@
+$(window).scroll(function () {
+                checkScroll()
+            });
+            function checkScroll() {
+                var am = $(".preload:not([class*='is-visible'])");
+                if ($(am).length > 0) {
+                    var h = $($(am)[0]).offset().top - $(window).scrollTop();
+                    if (h <= 500) {
+                        $($(am)[0]).addClass("is-visible")
+                    }
+                }
+            }
