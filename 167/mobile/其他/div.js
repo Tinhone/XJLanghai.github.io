47905755e35f1c22beea140015aefed52037e8a8
Tinhone <tinhone23333@gmail.com>
2021-07-23 17:21:13 +0800

    转移
    嗯...

--- 167/mobile/其他/div.js
+++
@@ -1,7 +0,0 @@
-var aa = ["a1","a2","a3","a4","a5"];
-function a(b) {
-	for (var i = aa.length-1; i >= 0; i--) {
-		document.getElementById(aa[i]).style.display="none";
-	}
-	document.getElementById(aa[b]).style.display="";
-}
