21dee78dc8bce5cd9849a95301fbe40eabf6f30c
Tinhone <tinhone23333@gmail.com>
2021-07-24 02:45:45 +0800

    测试-2
    就是测试啦

--- 167/其他/动画.js
+++ 167/其他/动画.js
@@ -5,7 +5,7 @@ $(window).scroll(function () {
                 var am = $(".preload:not([class*='is-visible'])");
                 if ($(am).length > 0) {
                     var h = $($(am)[0]).offset().top - $(window).scrollTop();
-                    if (h <= 100) {
+                    if (h <= 500) {
                         $($(am)[0]).addClass("is-visible")
                     }
                 }
