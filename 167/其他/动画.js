3e6cb88aa55790140ebaac314654d9714fbd72e2
Tinhone <tinhone23333@gmail.com>
2021-07-24 02:33:34 +0800

    跳转修复
    awa

--- 167/其他/动画.js
+++ 167/其他/动画.js
@@ -5,7 +5,7 @@ $(window).scroll(function () {
                 var am = $(".preload:not([class*='is-visible'])");
                 if ($(am).length > 0) {
                     var h = $($(am)[0]).offset().top - $(window).scrollTop();
-                    if (h <= 500) {
+                    if (h <= 100) {
                         $($(am)[0]).addClass("is-visible")
                     }
                 }
